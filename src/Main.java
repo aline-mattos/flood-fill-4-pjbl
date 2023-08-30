56f830102e24643d30fd202e244d3ce17deca91e
aline-mattos <abelomo95@gmail.com>
2023-08-30 16:38:42 -0300

    criando funcao imprimir

--- src/Main.java
+++ src/Main.java
@@ -13,8 +13,22 @@ public class Main {
     };
     public static void main(String[] args) {
 
+        imprimir();
     }
 
-    
+    public static void imprimir() {
+        System.out.println();
+
+        for (int linha = 0; linha < matriz.length; linha++){
+            StringBuilder itensLinha = new StringBuilder("[ ");
+            for (int coluna = 0; coluna < matriz[linha].length; coluna++){
+                itensLinha.append(matriz[linha][coluna]);
+                itensLinha.append(" ");
+            }
+            itensLinha.append("]");
+            System.out.println(itensLinha);
+        }
+    }
+
 }
 
